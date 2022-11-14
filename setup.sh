db9f36ce1e1b669430937ec91c17f5be80a38244
Teddy <teddy552@ymail.com>
2022-11-13 22:59:42 -0800

    Add kubernetes resources

--- setup.sh
+++ setup.sh
@@ -7,10 +7,7 @@ curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && \
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh && sudo usermod -aG docker $USER
+curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh && \
+sudo systemctl start docker && sudo usermod -aG docker $USER
 
-# Get Kubernetes yaml files
-cd ..
-mkdir kubernetes-yaml && cd kubernetes-yaml
-git clone 
 sudo systemctl reboot
